98eb570ad6986cc49de16d61749ffec8003b0c96
Henrysam22 <samuel22adetula@gmail.com>
2022-12-15 18:14:25 -0800

    new tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplys two numbers
+ * @a: first parameter 
+ * @b: second parameter 
+ * Return: multiplied value
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
