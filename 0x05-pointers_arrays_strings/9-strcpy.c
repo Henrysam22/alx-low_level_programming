ff65de8c9d19de817bc0c4e25d61aac66ff0ae9d
Henrysam22 <samuel22adetula@gmail.com>
2022-12-19 19:17:54 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,13 +10,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int len = 0;
 
-	for (i = 0; *(src + 1) != '\0'; i++)
+	while (*(src + len) != '\0')
 	{
-		dest[i] = *(src + 1);
+		*(dest + len) = *(src + len);
+		len++;
 	}
-	dest[i] = '\0';
 
+	*(dest + len) = '\0';
 	return (dest);
 }
