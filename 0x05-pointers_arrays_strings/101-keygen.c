c9e0cefcc40f963e75f6bb34f943f7381ab55257
Henrysam22 <samuel22adetula@gmail.com>
2022-12-19 20:23:57 -0800

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords
+ * return: 0 if success
+ */
+
+int main(void)
+{
+	int pass, sum;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += 128;
+		printf("%c", pass);
+	}
+
+	printf("%c", 2772 - sum);
+
+	return (0);
+}
