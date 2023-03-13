ca17a8441ebce4f79839a466f1d7a8925de2b7f8
Henrysam22 <samuel22adetula@gmail.com>
2023-03-12 19:37:36 -0700

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function prints the name of its executable file
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: program returns 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(int argc __attribute__((unused)), char *argv[]);
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int _atoi(char *s);
+int main(int argc, char *argv[]);
+
+#endif
