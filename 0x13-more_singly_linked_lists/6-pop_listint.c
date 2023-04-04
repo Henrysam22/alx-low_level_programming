305382c32c901ae7e71b5168ee21ac02a1df7a48
Henrysam22 <samuel22adetula@gmail.com>
2023-04-04 15:26:50 -0700

    pop_the_node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes a node
+ * @head: double pointer to the first node in the list
+ *
+ * Return: head node's data or 0 if empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp_node;
+	int value;
+
+	if (*head == NULL || head == NULL)
+		return (0);
+
+	temp = *head;
+	value = *head->n;
+	*head = *head->next;
+	free(temp);
+
+	return (value);
+}
