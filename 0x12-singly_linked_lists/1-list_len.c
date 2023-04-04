542a2ff5448cfbc6f5279f54972a49a47b0a4f94
Henrysam22 <samuel22adetula@gmail.com>
2023-04-03 21:01:32 -0700

    I can count the linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "lists.h"
+
+size_t list_len(const list_t *h)
+{
+	list_t *temp = h;
+	size_t count = 0;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
