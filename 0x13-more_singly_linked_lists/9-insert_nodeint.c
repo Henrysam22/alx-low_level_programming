f166556b4ce092e95c1ff574cc8d4d4d63eb1dfd
Henrysam22 <samuel22adetula@gmail.com>
2023-04-04 16:58:24 -0700

    getting node at the index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		temp = *head;
-		head = new;
+		*head = new;
 		new->next = temp;
 		return (new);
 	}
@@ -39,3 +39,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp = temp->next;
 			return (NULL);
 		}
+	}
+}
