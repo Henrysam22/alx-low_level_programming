a699762ff20e0e7f36aa00fd53e3a99cfba766e5
Henrysam22 <samuel22adetula@gmail.com>
2023-04-03 21:49:56 -0700

    count the list and add node at the end of the list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * list_len - counts the length of a linked list
+ * @h: pointer to the first node
+ *
+ * Return: return the length of the list
+ */
 
 size_t list_len(const list_t *h)
 {
-	list_t *temp = h;
+	const list_t *temp = h;
 	size_t count = 0;
 
 	while (temp != NULL)
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node_end - appends new node to the linked list
+ * @head: pointer to first node
+ * @str: string value of the new node
+ *
+ * Return: return address of the new node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int count = 0;
+	list_t *temp = *head;
+	const char *string = str;
+	list_t *new = (list_t *)malloc(sizeof(list_t));
+
+	while (string[1] != NULL)
+		count++;
+
+	if (new == NULL)
+		return (NULL);
+	new->len = count;
+	new->string = strdup(string);
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+		return (new);
+	}
+}
